66e6405ed9653a44886d039fe3a8054d056e0d7b
Eric Jalbert <ejalbert@Piano-ThinkPad.(none)>
2015-02-19 15:26:45 -0500

    Added Existing files

---
+++ Chapters/Show1DFigures/addMaxMin.py
@@ -0,0 +1,134 @@
+#! /usr/bin/env python
+
+f = open('2D_out1.txt','r')
+lines = f.readlines()
+f.close()
+
+total = 0
+for line in lines:
+  columns = line.split("  ")
+  total = total + abs(float(columns[6]) - float(columns[7]))
+  
+total = total/256.0
+print total
+  
+f = open('2D_out2.txt','r')
+lines = f.readlines()
+f.close()
+
+total = 0
+for line in lines:
+  columns = line.split("  ")
+  total = total + abs(float(columns[6]) - float(columns[7]))
+  
+total = total/256.0
+print total
+
+f = open('2D_out3.txt','r')
+lines = f.readlines()
+f.close()
+
+total = 0
+for line in lines:
+  columns = line.split("  ")
+  total = total + abs(float(columns[6]) - float(columns[7]))
+  
+total = total/256.0
+print total
+
+f = open('2D_out4.txt','r')
+lines = f.readlines()
+f.close()
+
+total = 0
+for line in lines:
+  columns = line.split("  ")
+  total = total + abs(float(columns[6]) - float(columns[7]))
+  
+total = total/256.0
+print total
+
+f = open('2D_out5.txt','r')
+lines = f.readlines()
+f.close()
+
+total = 0
+for line in lines:
+  columns = line.split("  ")
+  total = total + abs(float(columns[6]) - float(columns[7]))
+  
+total = total/256.0
+print total
+
+f = open('2D_out6.txt','r')
+lines = f.readlines()
+f.close()
+
+total = 0
+for line in lines:
+  columns = line.split("  ")
+  total = total + abs(float(columns[6]) - float(columns[7]))
+  
+total = total/256.0
+print total
+
+f = open('2D_out7.txt','r')
+lines = f.readlines()
+f.close()
+
+total = 0
+for line in lines:
+  columns = line.split("  ")
+  total = total + abs(float(columns[6]) - float(columns[7]))
+  
+total = total/256.0
+print total
+
+f = open('2D_out8.txt','r')
+lines = f.readlines()
+f.close()
+
+total = 0
+for line in lines:
+  columns = line.split("  ")
+  total = total + abs(float(columns[6]) - float(columns[7]))
+  
+total = total/256.0
+print total
+
+f = open('2D_out9.txt','r')
+lines = f.readlines()
+f.close()
+
+total = 0
+for line in lines:
+  columns = line.split("  ")
+  total = total + abs(float(columns[6]) - float(columns[7]))
+  
+total = total/256.0
+print total
+
+f = open('2D_out10.txt','r')
+lines = f.readlines()
+f.close()
+
+total = 0
+for line in lines:
+  columns = line.split("  ")
+  total = total + abs(float(columns[6]) - float(columns[7]))
+  
+total = total/256.0
+print total
+
+f = open('2D_out11.txt','r')
+lines = f.readlines()
+f.close()
+
+total = 0
+for line in lines:
+  columns = line.split("  ")
+  total = total + abs(float(columns[6]) - float(columns[7]))
+  
+total = total/256.0
+print total
+
